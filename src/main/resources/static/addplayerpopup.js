4bf2caa98897edf70d4c6b2d0b7e706dadfed92c
Admin <Admin@STUDENT04>
2020-01-07 18:11:36 +0000

    front end begins

---
+++ src/main/resources/static/addplayerpopup.js
@@ -0,0 +1,24 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
