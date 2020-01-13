5ea245ed60f04c48b52ce5bf1c4135a285d3ed49
qa-admin <qa-admin@qaadmin-HP-ProDesk-400-G3-SFF>
2020-01-13 16:22:49 +0000

    add game

--- src/main/resources/static/addplayerpopup.js
+++ src/main/resources/static/addplayerpopup.js
@@ -87,7 +87,7 @@ function makeTable(data) {
         
         const addGameForm = document.createElement("form");
         addGameForm.className = "form-container";
-        addGameForm.onsubmit = "addGameData(playerid)";
+        addGameForm.addEventListener= "submit",() => addGameDate(playerId);
         
         
         const addTitle = document.createElement("h1");
