620d9d7795dc324cd67f751be1cf640ae8c1f1d3
qa-admin <qa-admin@qaadmin-HP-ProDesk-400-G3-SFF>
2020-01-14 16:36:25 +0000

    First Attempt at League Table without sorting or played column

---
+++ src/main/resources/static/leagueTable.js
@@ -0,0 +1,76 @@
+function getAllPlayers(){
+    axios.get('/DartsProject/findPlayer')
+         .then(response => {console.log(response.data); makeTable(response.data);});
+  }
+
+
+
+function createLeagueTable(data){
+    let players = data;
+    let leagueBody = document.getElementById("leagueBody");
+
+    for (let x= 0; x < players.length; x++){
+
+        const row = document.createElement("tr");
+              row.className = "table-dark";
+              leagueBody.appendChild(row);
+        
+        const cell1 = document.createElement("th");
+
+        const playername = document.createTextNode(players[x].playerName);
+              cell1.appendChild(playername);
+              row.appendChild(cell1);
+
+/*         const cell2 = document.createElement("td");
+        let played = createTextNode(getPlayed(players[x].playerId).value);
+            cell2.appendChild(played);
+            row.appendChild(cell2); */
+
+        const cell3 = document.createElement("td");
+        let wins = createTextNode(getWins(players[x].playerId).value);
+            cell3.appendChild(wins);
+            row.appendChild(cell3);
+
+        const cell4 = document.createElement("td");
+        let draws = createTextNode(getDraws(players[x].playerId).value);
+            cell4.appendChild(draws);
+            row.appendChild(cell4);
+
+        const cell5 = document.createElement("td");
+        let losses = createTextNode(getLosses(players[x].playerId).value);
+            cell5.appendChild(losses);
+            row.appendChild(cell5);
+
+            const cell6 = document.createElement("td");
+            let points = createTextNode(getpoints(players[x].playerId).value);
+                cell6.appendChild(points);
+                row.appendChild(cell6);
+    }
+
+
+}
+
+
+function getPoints(data){
+    let playerId = data;
+
+    axios.get("/DartsProject/getPoints/" + playerId);
+}
+
+function getWins(data){
+    let playerId = data;
+
+    axios.get("/DartsProject/getWins/" + playerId);
+}
+
+function getDraws(data){
+    let playerId = data;
+
+    axios.get("DartsProject/getDraws/" + playerId);
+}
+
+function getLosses(data){
+    let playerId = data;
+
+    axios.get("DartsProject/getLoss" + playerId);
+}
